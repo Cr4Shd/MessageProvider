f7dfa0884196a5b43ad80d994247bc75705199b1
im-mniemeyer <im-mniemeyer@KA1613>
2023-06-09 18:07:44 +0200

    Kommentare

--- MessageProviderUnitTest/MessageEventArgs.cs
+++ MessageProviderUnitTest/MessageEventArgs.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace MessageProviderUnitTest
 {
     /// <summary>
-    /// Die Message
+    /// Die Messageklasse - <T> ist IPostClient
     /// </summary>
     /// <typeparam name="T"></typeparam>
     class MessageEventArgs<T>
@@ -16,6 +16,14 @@ namespace MessageProviderUnitTest
         public IEnumerable<string> Reciever { get; set; }
         public string Message { get; set; }
 
+        /// <summary>
+        /// Para0 = Das Senderobjekt
+        /// Para1 = Ein IEnumerableObjekt<string> mit den Empfängern (exakt selber Name mit welchem die Senderobjekte registriert wurden
+        /// Para2 = Die Nachricht als String
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="reciever"></param>
+        /// <param name="message"></param>
         public MessageEventArgs(T sender, IEnumerable<string> reciever, string message)
         {
             Sender = sender;
